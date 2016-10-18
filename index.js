59806e4d29752b4bf5b3a90ce73718a74ee4b3d0
albertkim <albert275@gmail.com>
2016-10-17 22:13:47 -0700

    Working chart for part a

--- index.js
+++ index.js
@@ -1,15 +1,30 @@
 console.log('Initializing javascript...')
 
-console.log(smallArray)
-console.log(bigArray)
-
-const array = smallArray
+const array = bigArray
 
 const calculatedArray = []
+let numberOfOnesUpToCurrent = 0
+let numberOfAcceptedSamples = 0
+for (let i=1; i<array.length; i++) {
+  if (array[i] !== -1) {
+    numberOfAcceptedSamples ++
+  }
+  if (array[i] === 1) {
+    numberOfOnesUpToCurrent ++
+  }
+  calculatedArray.push(numberOfOnesUpToCurrent/numberOfAcceptedSamples)
+}
 
-array.forEach(number => {
+// Print the final value
+console.log('Final value: ' + calculatedArray[calculatedArray.length-1])
 
-})
+const calculatedXYArray = []
+for (let i=1; i<calculatedArray.length; i++) {
+  calculatedXYArray.push({
+    x: i,
+    y: calculatedArray[i]
+  })
+}
 
 $(document).ready(function(){
   renderChart()
@@ -21,17 +36,22 @@ function renderChart() {
   var myChart = new Chart(ctx, {
     type: 'line',
     data: {
-      labels: new Array(array.length),
+      labels: new Array(calculatedArray.length),
       datasets: [{
-        label: '# of Votes',
-        data: array,
+        label: '# of samples',
+        data: calculatedXYArray,
         backgroundColor: 'rgba(255, 99, 132, 0.2)',
         borderColor: 'rgba(255,99,132,1)',
         borderWidth: 1
       }]
     },
     options: {
-
+      scales: {
+        xAxes: [{
+          type: 'logarithmic',
+          position: 'bottom'
+        }]
+      }
     }
   })
 }
